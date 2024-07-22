17492f7b3e50312f4e8e1aea917f59bd0b29a699
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2024-07-22 17:57:47 -0300

    Melhoria do layout

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ from utils import *
 
 load_dotenv()
 
+aux = False
 
-def final_func():
-    response1 = "Se precisar de mais informações ou quiser falar diretamente com nossa equipe, entre em contato:\n- **Email:** hubinovacao@insper.edu.br\n- **Telefone:** (11) 1234-5678\n- **Website:** [www.insper.edu.br/hub](http://www.insper.edu.br/hub) \n\nObrigado por utilizar o chatbot do Hub de Inovação do Insper!"   
-    st.session_state.chat_history.append(AIMessage(content=response1))
+@st.experimental_dialog("Obrigado por utilizar o chatbot do Hub de Inovação do Insper!")
+def vote():
+    response1 = "Se precisar de mais informações ou quiser falar diretamente com nossa equipe, entre em contato:\n- **Email:** hubinovacao@insper.edu.br\n- **Telefone:** (11) 1234-5678\n- **Website:** [www.hub.insper.edu.br](http://www.insper.edu.br/hub)"   
+    st.write(response1)
 
 embedding_size = 3072
 embedding_model = 'text-embedding-3-large'
@@ -51,16 +53,33 @@ for message in st.session_state.chat_history:
             st.write(message.content)
 
 # user input
-user_query = st.chat_input("Type your message here...")
+
+user_query = st.chat_input("Digite algo...")
+
+def verifica(chat_history):
+    
+        if isinstance(chat_history[-1], HumanMessage):
+            if formata(chat_history[-1].content) == "nao":
+                vote()
+                aux = False
+                return True
+
 if user_query is not None and user_query != "":
+    aux = True
     st.session_state.chat_history.append(HumanMessage(content=user_query))
 
     with st.chat_message("Human", avatar="👤"):
         st.markdown(user_query)
 
-    with st.chat_message("AI", avatar="🤖"):
-        with st.spinner("Thinking..."):
-            response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
-            st.write("Mais alguma dúvida?")        
-            st.button('Encerrar Chat', on_click=final_func)
-    st.session_state.chat_history.append(AIMessage(content=response))
+    if not (verifica(st.session_state.chat_history)) and aux:
+        with st.chat_message("AI", avatar="🤖"):
+            with st.spinner("Thinking..."):
+                response = st.write_stream(respond(user_query, st.session_state.chat_history, st.session_state.db, st.session_state.retriever))
+                aux = True
+                response2 = "Você tem mais alguma dúvida?"
+                st.write(response2)
+                st.session_state.chat_history.append(AIMessage(content=response))
+                st.session_state.chat_history.append(AIMessage(content=response2))
+            
+    
+
